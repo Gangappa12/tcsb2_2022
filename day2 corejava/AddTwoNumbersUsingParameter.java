5442307171ab5367ddbccc7802992eca75e518a9
Gangappa <ghubballi98@gmail.com>
2022-05-19 12:11:57 +0000

    my first commit

---
+++ day2 corejava/AddTwoNumbersUsingParameter.java
@@ -0,0 +1,21 @@
+package com.demo.add;
+
+class AddTwoNumbersParameter {
+
+void AddDemo(int number1, int number2) {
+		
+		int result =number1+number2;
+		System.out.println("sum of two numbers "+result);
+	}
+}
+	
+public class AddTwoNumbersUsingParameter {
+	
+	public static void main(String[]args) {
+		
+		AddTwoNumbersParameter b= new AddTwoNumbersParameter();// created object here
+		b.AddDemo(45,45);//passing parameter here
+	}
+
+}
+ 
